ccc0cf0e42a6d973046c5af46f642bd953dcde2f
Xinyuan <niqnnikki@gmail.com>
2025-04-15 17:49:17 +0200

    Initial commit

--- isaaclab/scripts/reinforcement_learning/skrl/train.py
+++ isaaclab/scripts/reinforcement_learning/skrl/train.py
@@ -20,10 +20,10 @@ from isaaclab.app import AppLauncher
 # add argparse arguments
 parser = argparse.ArgumentParser(description="Train an RL agent with skrl.")
 parser.add_argument("--video", action="store_true", default=False, help="Record videos during training.")
-parser.add_argument("--video_length", type=int, default=200, help="Length of the recorded video (in steps).")
-parser.add_argument("--video_interval", type=int, default=2000, help="Interval between video recordings (in steps).")
+parser.add_argument("--video_length", type=int, default=400, help="Length of the recorded video (in steps).")
+parser.add_argument("--video_interval", type=int, default=2500, help="Interval between video recordings (in steps).")
 parser.add_argument("--num_envs", type=int, default=None, help="Number of environments to simulate.")
-parser.add_argument("--task", type=str, default=None, help="Name of the task.")
+parser.add_argument("--task", type=str, default='Isaac-Direct-Dual-Cube-Franka-OSC-v0', help="Name of the task.")
 parser.add_argument("--seed", type=int, default=None, help="Seed used for the environment")
 parser.add_argument(
     "--distributed", action="store_true", default=False, help="Run training with multiple GPUs or nodes."
@@ -35,15 +35,14 @@ parser.add_argument(
     type=str,
     default="torch",
     choices=["torch", "jax", "jax-numpy"],
-    help="The ML framework used for training the skrl agent.",
-)
+    help="The ML framework used for training the skrl agent.", ) 
 parser.add_argument(
-    "--algorithm",
-    type=str,
-    default="PPO",
-    choices=["AMP", "PPO", "IPPO", "MAPPO"],
-    help="The RL algorithm used for training the skrl agent.",
-)
+        "--algorithm",
+        type=str,
+        default="PPO",
+        choices=["AMP", "PPO", "IPPO", "MAPPO"],
+        help="The RL algorithm used for training the skrl agent.",
+    )
 
 # append AppLauncher cli args
 AppLauncher.add_app_launcher_args(parser)
@@ -71,7 +70,7 @@ import skrl
 from packaging import version
 
 # check for minimum supported skrl version
-SKRL_VERSION = "1.4.2"
+SKRL_VERSION = "1.4.1"
 if version.parse(skrl.__version__) < version.parse(SKRL_VERSION):
     skrl.logger.error(
         f"Unsupported skrl version: {skrl.__version__}. "
@@ -84,6 +83,8 @@ if args_cli.ml_framework.startswith("torch"):
 elif args_cli.ml_framework.startswith("jax"):
     from skrl.utils.runner.jax import Runner
 
+from isaaclab_rl.skrl import SkrlVecEnvWrapper
+
 from isaaclab.envs import (
     DirectMARLEnv,
     DirectMARLEnvCfg,
@@ -95,12 +96,8 @@ from isaaclab.utils.assets import retrieve_file_path
 from isaaclab.utils.dict import print_dict
 from isaaclab.utils.io import dump_pickle, dump_yaml
 
-from isaaclab_rl.skrl import SkrlVecEnvWrapper
-
 import isaaclab_tasks  # noqa: F401
 from isaaclab_tasks.utils.hydra import hydra_task_config
-
-# PLACEHOLDER: Extension template (do not remove this comment)
 
 # config shortcuts
 algorithm = args_cli.algorithm.lower()
